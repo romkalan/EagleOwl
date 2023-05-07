d954f2dfd2c58e644d420531c4401fecfa86946a
romkalan <romkalan@mail.ru>
2023-05-08 01:17:24 +0300

    Initial Commit

---
+++ EagleOwl/EagleOwlApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleOwlApp.swift
+//  EagleOwl
+//
+//  Created by Roman Lantsov on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EagleOwlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
